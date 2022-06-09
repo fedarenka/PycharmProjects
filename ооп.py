47011a5477aaf8c2b6639ef85279ad81380cbcad
Svetlana <s.fedarenka@mail.ru>
2022-06-09 22:12:57 +0300

    вариант 1

--- ооп.py
+++ ооп.py
@@ -1,10 +1,10 @@
 import random
 
 class Head:
-    eyes = 2
-    ears = 2
-    mouth = 1
-    def eat(self):
+    eyes = 2 # статические свойства
+    ears = 2 # статические свойства
+    mouth = 1 # статические свойства
+    def eat(self): # метод
         taste = "вкусно" if random.randint(0,1) else "не вкусно"
         print(taste)
 
@@ -14,9 +14,39 @@ nick.eat()
 
 class Body:
     heart = 1
+
     def sweat(self):
         sweat_ml = random.randint(10, 100)
         print(f'выделилось {sweat_ml} мл пота')
+
+    def __init__(self, height=180, weight=80, sex = "f"):
+        self.height=height
+        self.weight=weight
+        self.sex =sex
+        self.childern = 0
+
+    def make_childern(self, childern_numer):
+        if self.sex =="f":
+            self.gain_weight()
+            self.childern+=1
+        else:
+            print("вы не женщина")
+
+    def gain_weight(self):
+        self.weight+=random.randint(1, 10)
+        print("вес -", self.weight)
+
+
+
+
+
+dudy = Body(height=160, weight=60)
+print(dudy.height)
+print(dudy.weight)
+dudy.gain_weight()
+dudy.make_childern(4)
+print(dudy.childern)
+
 class Human(Head, Body):
     pass
 nick = Human()
@@ -50,3 +80,45 @@ class ears:
 
 nick = ears()
 nick.listen()
+
+# динамические свойства
+
+class Head:
+    eyes = 2  # статические свойства
+    ears = 2  # статические свойства
+    mouth = 1  # статические свойства
+    def __init__(self, eye_color, teeth_number): # динамические свойства
+        self.eye_color = eye_color
+        self.teeth_number = teeth_number
+    def eat(self):  # метод
+        taste = 'вкусно' if random.randint(0,1) else 'не вкусно'
+        print(taste)
+professor = Head('green', 25)
+print(professor.eye_color)
+print(professor.teeth_number)
+
+class Head:
+    eyes = 2  # статические свойства
+    ears = 2  # статические свойства
+    mouth = 1  # статические свойства
+    def __init__(self, eye_color='blue', teeth_number=32): # динамические свойства
+        self.eye_color = eye_color
+        self.teeth_number = teeth_number
+    def eat(self):  # метод
+        taste = 'вкусно' if random.randint(0,1) else 'не вкусно'
+        print(taste)
+    def delete_tooth(self):  # метод
+        if self.teeth_number >= 1:
+            self.teeth_number -= 1
+            print(f'Зуб удален. Осталось {self.teeth_number} зубов')
+        else:
+            print('Зубов нет!')
+    def become_van_goh(self):  # метод
+        if self.ears == 2:
+            self.ears -= 1
+            print(f'Ухо удалено. Теперь вы почти Ван Гог!')
+        elif self.ears == 1:
+            print(f'Вы и так почти Ван Гог! Ухо последнее не трогайте.')
+
+class Body:
+    hand = 2
